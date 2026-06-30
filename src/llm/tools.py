f930476108de2f0e9dd05117119f51d9662b91e0
Nevin Abraham <nevinabr025@gmail.com>
2026-07-01 00:51:26 +0530

    Feat: Tools updated and tested

--- src/llm/tools.py
+++ src/llm/tools.py
@@ -7,12 +7,59 @@ All tools available to FinGuard AI.
 from src.services.prediction_service import predict_credit_risk
 
 
-def credit_risk_tool(user_input: dict):
+def credit_risk_tool(
+    LIMIT_BAL,
+    SEX,
+    EDUCATION,
+    MARRIAGE,
+    AGE,
+    PAY_0,
+    PAY_2,
+    PAY_3,
+    PAY_4,
+    PAY_5,
+    PAY_6,
+    BILL_AMT1,
+    BILL_AMT2,
+    BILL_AMT3,
+    BILL_AMT4,
+    BILL_AMT5,
+    BILL_AMT6,
+    PAY_AMT1,
+    PAY_AMT2,
+    PAY_AMT3,
+    PAY_AMT4,
+    PAY_AMT5,
+    PAY_AMT6,
+):
     """
-    Tool that predicts
-    customer's credit risk.
+    Predict credit risk for a customer.
     """
 
-    result = predict_credit_risk(user_input)
+    customer = {
+        "LIMIT_BAL": LIMIT_BAL,
+        "SEX": SEX,
+        "EDUCATION": EDUCATION,
+        "MARRIAGE": MARRIAGE,
+        "AGE": AGE,
+        "PAY_0": PAY_0,
+        "PAY_2": PAY_2,
+        "PAY_3": PAY_3,
+        "PAY_4": PAY_4,
+        "PAY_5": PAY_5,
+        "PAY_6": PAY_6,
+        "BILL_AMT1": BILL_AMT1,
+        "BILL_AMT2": BILL_AMT2,
+        "BILL_AMT3": BILL_AMT3,
+        "BILL_AMT4": BILL_AMT4,
+        "BILL_AMT5": BILL_AMT5,
+        "BILL_AMT6": BILL_AMT6,
+        "PAY_AMT1": PAY_AMT1,
+        "PAY_AMT2": PAY_AMT2,
+        "PAY_AMT3": PAY_AMT3,
+        "PAY_AMT4": PAY_AMT4,
+        "PAY_AMT5": PAY_AMT5,
+        "PAY_AMT6": PAY_AMT6,
+    }
 
-    return result
+    return predict_credit_risk(customer)
