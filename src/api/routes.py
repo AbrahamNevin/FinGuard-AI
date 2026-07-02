ada1f8c4ef2b0a1d155f14abb0203ba1495f8062
Nevin Abraham <nevinabr025@gmail.com>
2026-07-03 00:50:15 +0530

    Feat: Endpoint returns predictions successfully

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,9 +5,15 @@ API endpoints for FinGuard AI.
 """
 
 from fastapi import APIRouter
+from src.api.schemas import PredictionRequest
+from src.services.prediction_service import predict_credit_risk
 
 router = APIRouter()
 
+
+# --------------------------------------------------
+# Ping
+# --------------------------------------------------
 
 @router.get("/ping")
 def ping():
@@ -17,3 +23,17 @@ def ping():
         "message": "FinGuard API Working"
 
     }
+
+
+# --------------------------------------------------
+# Predict Credit Risk
+# --------------------------------------------------
+
+@router.post("/predict")
+def predict(request: PredictionRequest):
+
+    customer = request.customer.model_dump()
+
+    result = predict_credit_risk(customer)
+
+    return result
